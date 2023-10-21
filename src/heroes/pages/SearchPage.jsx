b4de9845baf5b53beab11a7cc030572af7058664
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-21 03:56:51 -0700

    Mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -19,6 +19,9 @@ export const SearchPage = () => {
     
     const heroes = useMemo(()=>getHeroesByName(q),[q]);
 
+    const showSearch = (q.length === 0);
+    const showError = (q.length > 0) && heroes.length === 0;
+
     const {searchText,onInputChange} = useForm({
         searchText:q
     });
@@ -26,7 +29,7 @@ export const SearchPage = () => {
     const onSearchSubmit = (event) => {
         // preventDefault(); EVITAR PROPAGACION DEL FORMULARIO
         event.preventDefault();
-        if(searchText.trim().length <= 1) return;
+        //if(searchText.trim().length <= 1) return;
         navigate(`?q=${searchText}`)
     }
 
@@ -58,14 +61,15 @@ export const SearchPage = () => {
                 <h4>Results</h4>
                 <hr/>
 
-                <div className="alert alert-primary">
-                    Search a hero
-                </div>
+                {
+                    /*
+                    (q === '') ? <div className="alert alert-primary"> Search a hero </div>
+                    : (heroes.length === 0) && <div className="alert alert-danger"> No hero with <b>{q}</b></div>
+                    */
+                }
+                <div className="alert alert-primary animate__animated animate__fadeIn" style={{display: showSearch ? '' : 'none'}}> Search a hero </div>
 
-                <div className="alert alert-danger">
-                    No hero with <b>{q}</b>
-                </div>
-
+                <div className="alert alert-danger animate__animated animate__fadeIn" style={{display: showError ? '' : 'none'}}> No hero with <b>{q}</b></div>
                 {
                     heroes.map(hero => (
                         <HeroCard key={hero.id} {...hero}/>
