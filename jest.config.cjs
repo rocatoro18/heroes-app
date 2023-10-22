ef2e479976d05a50cd82c201a7b123ab312c7ee7
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-22 16:07:30 -0700

    init test

---
+++ jest.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    // NO HAY NADA QUE CONFIGURAR DE ANTEMANO PARA LA
+    // PARTE DEL JEST, como del fetch api o mock global
+    //setupFiles: ['./jest.setup.js']
+}
