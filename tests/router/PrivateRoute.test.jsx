73383aaa22f21104a6c0722ecd790ecb6786abdc
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-10-22 19:42:24 -0700

    Pruebas en PrivateRoutes

---
+++ tests/router/PrivateRoute.test.jsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import { AuthContext } from '../../src/auth';
+import { PrivateRoute } from '../../src/router/PrivateRoute';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en el <PrivateRoute />',()=>{
+    test('debe de mostrar el children si esta autenticado',()=>{
+        
+        // SIMULAMOS EL LOCAL STORAGE PARA EVALUARLO
+        Storage.prototype.setItem = jest.fn();
+
+        const contextValue = {
+            logged: true,
+            user: {
+                id: '18111999',
+                name: 'Roberto'
+            }
+        }
+
+        render(
+        // ESTABLECER CONTEXTO
+        <AuthContext.Provider value={contextValue}>
+            {/* PROPORCIONAMOS EL ROUTER */}
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <PrivateRoute>
+                    <h1>Ruta Privada</h1>
+                </PrivateRoute>
+            </MemoryRouter>
+        </AuthContext.Provider>
+       )
+        
+       //screen.debug();
+
+       expect(screen.getByText('Ruta Privada')).toBeTruthy();
+        expect(localStorage.setItem).toHaveBeenCalledWith('lastPath','/search?q=batman');
+
+    });
+})
